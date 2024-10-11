246216d579e6bac77958c5e32f15439ae9c87369
Md Nasemul Islam <83567564+nasemul1@users.noreply.github.com>
2024-10-12 01:19:39 +0600

    fixed color in nav and hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,11 +5,11 @@ function Hero() {
     <div className='col-start-2 col-span-10'>
         <div >
             <p className='font-bold text-[24px]'>Hi I am</p>
-            <p>Mohammad Umair</p>
-            <p>UI & UX</p>
+            <p className='font-bold text-[32px] text-[#FD6F00]'>Mohammad Umair</p>
+            <p className='font-bold text-[100px]'>UI & UX</p>
             <p>Designer</p>
             <p>Lorem ipsum dolor sit amet consectetur. Tristique amet sed massa nibh lectus netus in. Aliquet donec morbi convallis pretium. Turpis tempus pharetra</p>
-            <button className='bg-orange-500 text-white px-4 py-2 rounded-md hover:bg-orange-600'>Hire Me</button>
+            <button className='bg-[#FD6F00] text-white px-4 py-2 rounded-md hover:bg-orange-600'>Hire Me</button>
         </div>
         <div></div>
     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,19 +5,19 @@ function Navbar() {
     <>
         <nav className="col-start-2 col-span-10 flex justify-between items-center p-4 bg-white text-[21px]">
             <div className="flex items-center gap-x-2">
-                <div className='bg-orange-500 w-[40px] h-[40px] rounded-full flex justify-center items-center'>
+                <div className='bg-[#FD6F00] w-[40px] h-[40px] rounded-full flex justify-center items-center'>
                     <p className='font-satisfy font-bold text-[28px] text-white'>mu</p>
                 </div>
                 <span className="text-2xl text-[#545454]"><span className='text-[#1E1E1E] font-bold'>M</span>umair</span>
             </div>
             <ul className="flex items-center space-x-6 font-medium text-[#1E1E1E]">
-                <li><a href="#home" className="hover:text-orange-500">Home</a></li>
-                <li><a href="#about" className="hover:text-orange-500">About Me</a></li>
-                <li><a href="#services" className="hover:text-orange-500">Services</a></li>
-                <li><a href="#projects" className="hover:text-orange-500">Projects</a></li>
-                <li><a href="#testimonials" className="hover:text-orange-500">Testimonials</a></li>
-                <li><a href="#contact" className="hover:text-orange-500">Contact</a></li>
-                <button className="bg-orange-500 text-white px-4 py-2 rounded-md hover:bg-orange-600">
+                <li><a href="#home" className="hover:text-[#FD6F00]">Home</a></li>
+                <li><a href="#about" className="hover:text-[#FD6F00]">About Me</a></li>
+                <li><a href="#services" className="hover:text-[#FD6F00]">Services</a></li>
+                <li><a href="#projects" className="hover:text-[#FD6F00]">Projects</a></li>
+                <li><a href="#testimonials" className="hover:text-[#FD6F00]">Testimonials</a></li>
+                <li><a href="#contact" className="hover:text-[#FD6F00]">Contact</a></li>
+                <button className="bg-[#FD6F00] text-white px-4 py-2 rounded-md hover:bg-orange-600">
                     Download CV
                 </button>
             </ul>
