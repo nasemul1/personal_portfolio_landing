2df7e174fb06737d57e4f4c3ffecc291f3537509
Md Nasemul Islam <83567564+nasemul1@users.noreply.github.com>
2024-10-12 00:35:05 +0600

    completed navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,14 +3,14 @@ import React from 'react'
 function Navbar() {
   return (
     <>
-        <nav className="col-start-2 col-span-10 flex justify-between items-center p-4 bg-gray-100 text-sm">
-            <div className="flex items-center">
-                <div className=''>
-                    <p className='font-qwitcher font-bold text-[42px]'>mu</p>
+        <nav className="col-start-2 col-span-10 flex justify-between items-center p-4 bg-white text-sm">
+            <div className="flex items-center gap-x-2">
+                <div className='bg-orange-500 w-[40px] h-[40px] rounded-full flex justify-center items-start'>
+                    <p className='font-qwitcher font-bold text-[46px] text-white p-1'>mu</p>
                 </div>
                 <span className="text-2xl text-[#545454]"><span className='text-[#1E1E1E] font-bold'>M</span>umair</span>
             </div>
-            <ul className="flex items-center space-x-6 font-medium text-[#1E1E1E] text-[13px]">
+            <ul className="flex items-center space-x-6 font-medium text-[#1E1E1E]">
                 <li><a href="#home" className="hover:text-orange-500">Home</a></li>
                 <li><a href="#about" className="hover:text-orange-500">About Me</a></li>
                 <li><a href="#services" className="hover:text-orange-500">Services</a></li>
