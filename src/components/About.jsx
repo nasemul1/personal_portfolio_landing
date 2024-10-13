bcc58dc882f51193171a66d0ebfd4522c5165b05
Md Nasemul Islam <83567564+nasemul1@users.noreply.github.com>
2024-10-13 20:53:01 +0600

    added about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,8 +3,8 @@ import model from '../assets/model.webp'
 
 function About() {
   return (
-    <div className='col-start-2 col-span-10'>
-        <div>
+    <div className='col-start-2 col-span-10 flex items-center justify-between'>
+        <div className=''>
             <div className='flex  items-end justify-center'>
                 <div className='z-10 flex flex-col items-center relative'>
                     <div className='w-[374px] h-[83px] absolute top-[13%] bg-[#FD6F00] bg-opacity-60'></div>
@@ -16,7 +16,20 @@ function About() {
                 </div>
             </div>
         </div>
-        <div></div>
+        <div className='w-[60%]'>
+            <p className='text-[65px] font-bold'>About Me</p>
+            <p className='text-[21px]'>Lorem ipsum dolor sit amet consectetur. Tristique amet sed massa nibh lectus netus in. Aliquet donec morbi convallis pretium. Turpis tempus pharetra</p>
+            <div className='my-5'>
+                <label htmlFor="ux-r" className='text-[24px] font-bold block'>UX</label>
+                <input type="range" min={0} max="100" value="90" id='ux-r' className="range w-full accent-orange-500" />
+                <label htmlFor="web-r" className='text-[24px] font-bold block'>Website Design</label>
+                <input type="range" min={0} max="100" value="80" id='web-r' className="range w-full accent-orange-500" />
+                <label htmlFor="app-r" className='text-[24px] font-bold block'>App Design</label>
+                <input type="range" min={0} max="100" value="95" id='app-r' className="range w-full accent-orange-500" />
+                <label htmlFor="ux-r" className='text-[24px] font-bold block'>Graphics Design</label>
+                <input type="range" min={0} max="100" value="90" id='ux-r' className="range w-full accent-orange-500" />
+            </div>
+        </div>
     </div>
   )
 }
