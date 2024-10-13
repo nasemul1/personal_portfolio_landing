5c9107e609036a3876dc37aa0b575344efc475b6
Md Nasemul Islam <83567564+nasemul1@users.noreply.github.com>
2024-10-13 21:38:42 +0600

    added project page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import About from './components/About'
+import Project from './components/Project'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Hero />
       <About />
+      <Project />
     </div>
   )
 }
---
+++ src/components/Project.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import p1 from '../assets/1.png'
+import p2 from '../assets/3.png'
+import p3 from '../assets/2.png'
+
+function Project() {
+  return (
+    <div className='my-20 col-start-2 col-span-10 flex flex-col justify-center'>
+        <p className='text-[65px] text-center font-semibold'>My Projects</p>
+        <p className='text-center text-[21px] my-5'>Lorem ipsum dolor sit amet consectetur. Mollis erat duis aliquam mauris est risus lectus. Phasellus consequat urna tellus</p>
+        <div className='my-5 mx-auto flex gap-x-3'>
+            <button className='px-4 py-3 border rounded-xl text-semibold text-[24px] '>All</button>
+            <button className='px-4 py-3 border rounded-xl text-semibold text-[24px] '>UI/UX</button>
+            <button className='px-4 py-3 border rounded-xl bg-orange-500 text-white text-semibold text-[24px] '>Web design</button>
+            <button className='px-4 py-3 border rounded-xl text-semibold text-[24px] '>App Design</button>
+            <button className='px-4 py-3 border rounded-xl text-semibold text-[24px] '>Graphics Design</button>
+        </div>
+        <div className='mx-auto my-10 flex gap-x-10'>
+            <div>
+                <div className='w-[485px] h-[489px] bg-[#FFEBDB] rounded-lg overflow-hidden relative'>
+                    <img src={p1} alt='project 1' className='w-[274px] absolute top-28 left-10 z-10'/>
+                    <img src={p1} alt='project 1' className='w-[274px] absolute top-[-300px] left-40'/>
+                </div>
+                <p className='text-[19px] text-orange-500 font-medium mt-5 mb-3'>Web Design</p>
+                <p className='text-[24px] font-semibold'>AirCalling Landing Page Design </p>
+            </div>
+            <div>
+                <div className='w-[485px] h-[489px] bg-[#FFEBDB] rounded-lg overflow-hidden relative'>
+                    <img src={p2} alt='project 1' className='w-[274px] absolute top-28 left-10 z-10'/>
+                    <img src={p2} alt='project 1' className='w-[274px] absolute top-[-300px] left-40'/>
+                </div>
+                <p className='text-[19px] text-orange-500 font-medium mt-5 mb-3'>Web Design</p>
+                <p className='text-[24px] font-semibold'>AirCalling Landing Page Design </p>
+            </div>
+            <div>
+                <div className='w-[485px] h-[489px] bg-[#FFEBDB] rounded-lg overflow-hidden relative'>
+                    <img src={p3} alt='project 1' className='w-[274px] absolute top-28 left-10 z-10'/>
+                    <img src={p3} alt='project 1' className='w-[274px] absolute top-[-300px] left-40'/>
+                </div>
+                <p className='text-[19px] text-orange-500 font-medium mt-5 mb-3'>Web Design</p>
+                <p className='text-[24px] font-semibold'>AirCalling Landing Page Design </p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Project
